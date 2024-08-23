95b5558c8d0226738bfaaa726e6e2fbd865244c1
YesuBalla <YesuBalla>
2024-08-23 17:42:43 +0530

    cors installed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+const cors = require('cors');
+app.use(cors());
 
 const {open} = require('sqlite');
 const sqlite3 = require('sqlite3');
